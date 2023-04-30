851847e0af166bcfb619a692900d258e8107f3d2
j8won <joowoni99@gmail.com>
2023-04-30 14:42:13 +0900

    feat: Logo.jsx 생성

--- src/client/src/components/common/Logo.jsx
+++ src/client/src/components/common/Logo.jsx
@@ -1,13 +1,26 @@
+import React from "react";
+import { useNavigate } from "react-router-dom";
 import styled from "styled-components";
+import routes from "../../routes.js";
 
 const StyledLogo = styled.div`
   font-family: ${({ theme }) => theme.fontFamily.logo};
   font-size: ${({ theme }) => theme.fontSize.logo};
   color: ${({ theme }) => theme.color.main};
+  cursor: pointer;
 `;
 
 function Logo() {
-  return <StyledLogo>Auttend</StyledLogo>;
+  const navigate = useNavigate();
+  return (
+    <StyledLogo
+      onClick={() => {
+        navigate(routes.home);
+      }}
+    >
+      Auttend
+    </StyledLogo>
+  );
 }
 
 export default Logo;
