f055e48828b75845dc06b78cee7caaec030e96d2
j8won <joowoni99@gmail.com>
2023-05-03 16:29:26 +0900

    feat: Form.jsx에 FormProvider 적용

--- src/client/src/components/common/Form.jsx
+++ src/client/src/components/common/Form.jsx
@@ -1,42 +1,49 @@
 import React from "react";
-import { useForm } from "react-hook-form";
+import { useForm, FormProvider } from "react-hook-form";
 import PropTypes from "prop-types";
 import styled from "styled-components";
+
 import LabeledInput from "./LabeledInput.jsx";
+import BlueButton from "./BlueButton.jsx";
 
 const StyledForm = styled.form`
   width: 350px;
 `;
 
 function Form({ onSubmit, onError, defaultValues, inputInformations }) {
-  const {
-    register,
-    handleSubmit,
-    formState: { errors },
-    getValues,
-  } = useForm({
+  const methods = useForm({
     mode: "onChange",
     defaultValues: defaultValues,
   });
 
-  return (
-    <StyledForm onSubmit={handleSubmit(onSubmit, onError)}>
-      {inputInformations.map((info) => (
-        <LabeledInput
-          key={info.id}
-          id={info.id}
-          label={info.label}
-          type={info.type}
-          value={getValues(info.id)}
-          register={register}
-          required={info?.required}
-          errorMessage={errors[info.id] && errors[info.id]?.message}
-          requireMessage={info?.requireMessage}
-        />
-      ))}
+  const {
+    handleSubmit,
+    formState: { errors },
+  } = methods;
 
-      <button type="submit">submit</button>
-    </StyledForm>
+  return (
+    <FormProvider {...methods}>
+      <StyledForm onSubmit={handleSubmit(onSubmit, onError)}>
+        {inputInformations.map((info) => (
+          <LabeledInput
+            key={info.id}
+            id={info.id}
+            label={info.label}
+            type={info.type}
+            required={info?.required}
+            errorMessage={errors[info.id] && errors[info.id]?.message}
+            requireMessage={info?.requireMessage}
+          />
+        ))}
+
+        <BlueButton
+          type="submit"
+          text="로그인"
+          width="100%"
+          style={{ marginTop: "1.5rem" }}
+        />
+      </StyledForm>
+    </FormProvider>
   );
 }
 
@@ -55,4 +62,4 @@ Form.propTypes = {
   ).isRequired,
 };
 
-export default Form;
+export default React.memo(Form);
