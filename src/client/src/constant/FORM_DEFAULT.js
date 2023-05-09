679af09618514791c41fd248c1651f12a7604cf3
j8won <joowoni99@gmail.com>
2023-05-10 08:37:06 +0900

    feat: CreateRoom.jsx 생성

--- src/client/src/constant/FORM_DEFAULT.js
+++ src/client/src/constant/FORM_DEFAULT.js
@@ -10,7 +10,13 @@ const SIGN_IN = {
   password: "",
 };
 
+const CREATE_ROOM = {
+  roomName: "",
+  roomPassword: "",
+};
+
 Object.freeze(SIGN_US);
 Object.freeze(SIGN_IN);
+Object.freeze(CREATE_ROOM);
 
-export default { SIGN_US, SIGN_IN };
+export default { SIGN_US, SIGN_IN, CREATE_ROOM };
