340977acb154521f051d8d2fd5d7b24f916df67b
j8won <joowoni99@gmail.com>
2023-05-18 02:12:28 +0900

    feat: 모달을 렌더링할 ModalPortal.jsx 생성

---
+++ src/client/src/components/common/modal/ModalPortal.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+function ModalPortal({ children }) {
+  const el = document.getElementById("modal");
+  return ReactDOM.createPortal(children, el);
+}
+
+export default ModalPortal;
+가;
