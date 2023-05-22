b1447e21fd53cfbfd6f8fa4f6099a42d02dd0e85
j8won <joowoni99@gmail.com>
2023-05-22 16:58:07 +0900

    feat: 출결 정보 페이지 생성

--- src/client/src/App.jsx
+++ src/client/src/App.jsx
@@ -13,6 +13,7 @@ import CreateRoom from "./pages/CreateRoom.jsx";
 import SignIn from "./pages/SignIn";
 import MyRoom from "./pages/MyRoom.jsx";
 import ManageRoom from "./pages/ManageRoom.jsx";
+import Attendance from "./pages/Attendance.jsx";
 
 function PrivateRouter() {
   return (
@@ -21,10 +22,7 @@ function PrivateRouter() {
       <Route path={routes.myPage} element={<div>my page</div>} />
       <Route path={routes.room} element={<div>Room</div>} />
       <Route path={routes.myRoom} element={<MyRoom />} />
-      <Route
-        path={`${routes.attendance}/:roomId`}
-        element={<div>Attendance</div>}
-      />
+      <Route path={`${routes.attendance}/:roomId`} element={<Attendance />} />
       <Route path={routes.createRoom} element={<CreateRoom />} />
       <Route path={`${routes.manageRoom}/:roomId`} element={<ManageRoom />} />
       <Route path="*" element={<div>error</div>} />
---
+++ src/client/src/pages/Attendance.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Layout from "../components/common/layout/Layout.jsx";
+
+function Attendance() {
+  return (
+    <Layout isLoggedIn={true} title="출결 정보">
+      <div>fsf</div>
+    </Layout>
+  );
+}
+
+export default Attendance;
