598e5be2ef5ee68bfa14a6d4eba9ab0d131cdd42
j8won <joowoni99@gmail.com>
2023-05-24 02:15:14 +0900

    feat: 1대1 영상통화 구현

--- src/client/src/pages/VideoCall.jsx
+++ src/client/src/pages/VideoCall.jsx
@@ -23,13 +23,17 @@ const UserVideoBox = styled.section`
 `;
 
 let myStream;
+let myPeerConnection;
+let myDataChannel;
 
 function VideoCall() {
+  const socket = io(import.meta.env.VITE_SIGNAL_SERVER);
+
   const { roomId } = useParams();
   const myVideoRef = useRef();
+  const otherVideoRef = useRef();
   const [cameraArray, setCameraArray] = useState([]);
   const [micArray, setMicArray] = useState([]);
-  // const socket = io(`${import.meta.env.VITE_SIGNAL_SERVER}video/${roomId}`);
 
   const initCall = async () => {
     myStream = await getMedia({
@@ -42,17 +46,104 @@ function VideoCall() {
 
   const joinRoom = async () => {
     await initCall();
+    await makeConnection();
+    socket.emit("join_room", roomId);
   };
 
   useEffect(() => {
     joinRoom();
+
+    return () => {
+      // socket.disconnect();
+    };
   }, []);
+
+  // RTC code
+  const makeConnection = async () => {
+    myPeerConnection = new RTCPeerConnection({
+      iceServers: [
+        {
+          urls: [
+            "stun:stun.l.google.com:19302",
+            "stun:stun1.l.google.com:19302",
+            "stun:stun2.l.google.com:19302",
+            "stun:stun3.l.google.com:19302",
+            "stun:stun4.l.google.com:19302",
+          ],
+        },
+      ],
+    });
+    myPeerConnection.addEventListener("icecandidate", handleIce);
+    await myStream
+      .getTracks()
+      .forEach((track) => myPeerConnection.addTrack(track, myStream));
+    myPeerConnection.addEventListener("track", handleTrack);
+  };
+
+  const handleIce = (data) => {
+    console.log("sent candidate");
+    socket.emit("ice", data.candidate, roomId);
+  };
+
+  const handleTrack = (data) => {
+    console.log("got an stream from my peer");
+    console.log("Peer's ", data.streams[0]);
+    console.log("My ", myStream);
+    console.log(data.streams);
+    otherVideoRef.current.srcObject = data.streams[0];
+  };
+
+  // socket
+  socket.on("welcome", async () => {
+    myDataChannel = myPeerConnection.createDataChannel("chat");
+    myDataChannel.addEventListener("message", (e) => console.log(e.data));
+    console.log("made data channel");
+
+    const offer = await myPeerConnection.createOffer();
+    await myPeerConnection.setLocalDescription(offer);
+    console.log("sent the offer");
+    socket.emit("offer", offer, roomId);
+  });
+
+  socket.on("offer", async (offer) => {
+    myPeerConnection.addEventListener("datachannel", (e) => {
+      myDataChannel = e.channel;
+      myDataChannel.addEventListener("message", (e) => console.log(e.data));
+    });
+    console.log("receive the offer");
+
+    await myPeerConnection.setRemoteDescription(offer);
+    const answer = await myPeerConnection.createAnswer();
+    await myPeerConnection.setLocalDescription(answer);
+    socket.emit("answer", answer, roomId);
+    console.log("sent the answer");
+  });
+
+  socket.on("answer", async (answer) => {
+    console.log("receive the answer");
+    await myPeerConnection.setRemoteDescription(answer);
+  });
+
+  socket.on("ice", async (ice) => {
+    console.log("received candidate");
+    await myPeerConnection.addIceCandidate(ice);
+  });
 
   return (
     <Container>
       <UserVideoBox>
         <video
           ref={myVideoRef}
+          autoPlay
+          playsInline
+          width="400px"
+          height="400px"
+        />
+      </UserVideoBox>
+
+      <UserVideoBox>
+        <video
+          ref={otherVideoRef}
           autoPlay
           playsInline
           width="400px"
--- src/signal-server/src/server.js
+++ src/signal-server/src/server.js
@@ -5,12 +5,31 @@ import SocketIO from "socket.io";
 const app = express();
 const httpServer = http.createServer(app);
 
-const PORT = 8080;
+const PORT = 3001;
 
 const wsServer = SocketIO(httpServer, {
   cors: {
     origin: "*",
   },
+});
+
+wsServer.on("connection", (socket) => {
+  socket.on("join_room", (roomId) => {
+    socket.join(roomId);
+    socket.to(roomId).emit("welcome");
+  });
+
+  socket.on("offer", (offer, roomId) => {
+    socket.to(roomId).emit("offer", offer);
+  });
+
+  socket.on("answer", (answer, roomId) => {
+    socket.to(roomId).emit("answer", answer);
+  });
+
+  socket.on("ice", (ice, roomId) => {
+    socket.to(roomId).emit("ice", ice);
+  });
 });
 
 httpServer.listen(PORT, () => {
