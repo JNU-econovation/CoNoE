15a54f757adbbd3c5cf83fcc44e62c8341990fa9
j8won <joowoni99@gmail.com>
2023-05-24 23:33:49 +0900

    feat: webRTC 관련 로직을 WebRTC.js로 분리

--- src/client/src/pages/VideoCall.jsx
+++ src/client/src/pages/VideoCall.jsx
@@ -5,6 +5,7 @@ import { io } from "socket.io-client";
 
 import NavBar from "../components/video-call/navigation-bar/NavBar.jsx";
 import getMedia from "../utils/getMedia.js";
+import { WebRTC } from "../utils/WebRTC.js";
 
 const Container = styled.main`
   width: 100%;
@@ -17,19 +18,16 @@ const Container = styled.main`
 const UserVideoBox = styled.section`
   video {
     transform: rotateY(180deg);
-    -webkit-transform: rotateY(180deg); /* Safari and Chrome */
-    -moz-transform: rotateY(180deg); /* Firefox */
   }
 `;
 
 let myStream;
-let myPeerConnection;
-let myDataChannel;
 
 function VideoCall() {
-  const socket = io(import.meta.env.VITE_SIGNAL_SERVER);
-
   const { roomId } = useParams();
+  const socket = io(import.meta.env.VITE_SIGNAL_SERVER);
+  const webRtc = new WebRTC(socket, roomId);
+
   const myVideoRef = useRef();
   const otherVideoRef = useRef();
   const [cameraArray, setCameraArray] = useState([]);
@@ -44,6 +42,12 @@ function VideoCall() {
     });
   };
 
+  const makeConnection = async () => {
+    webRtc.setIceCandidate();
+    await webRtc.addTracks(myStream);
+    webRtc.setRemoteVideo(otherVideoRef);
+  };
+
   const joinRoom = async () => {
     await initCall();
     await makeConnection();
@@ -53,81 +57,19 @@ function VideoCall() {
   useEffect(() => {
     joinRoom();
 
+    socket.on("welcome", webRtc.setLocalOffer);
+    socket.on("offer", webRtc.getRemoteOffer);
+    socket.on("answer", webRtc.getRemoteAnswer);
+    socket.on("ice", webRtc.getRemoteIce);
+
     return () => {
       // socket.disconnect();
+      socket.off("welcome", webRtc.setLocalOffer);
+      socket.off("offer", webRtc.getRemoteOffer);
+      socket.off("answer", webRtc.getRemoteAnswer);
+      socket.off("ice", webRtc.getRemoteIce);
     };
   }, []);
-
-  // RTC code
-  const makeConnection = async () => {
-    myPeerConnection = new RTCPeerConnection({
-      iceServers: [
-        {
-          urls: [
-            "stun:stun.l.google.com:19302",
-            "stun:stun1.l.google.com:19302",
-            "stun:stun2.l.google.com:19302",
-            "stun:stun3.l.google.com:19302",
-            "stun:stun4.l.google.com:19302",
-          ],
-        },
-      ],
-    });
-    myPeerConnection.addEventListener("icecandidate", handleIce);
-    await myStream
-      .getTracks()
-      .forEach((track) => myPeerConnection.addTrack(track, myStream));
-    myPeerConnection.addEventListener("track", handleTrack);
-  };
-
-  const handleIce = (data) => {
-    console.log("sent candidate");
-    socket.emit("ice", data.candidate, roomId);
-  };
-
-  const handleTrack = (data) => {
-    console.log("got an stream from my peer");
-    console.log("Peer's ", data.streams[0]);
-    console.log("My ", myStream);
-    console.log(data.streams);
-    otherVideoRef.current.srcObject = data.streams[0];
-  };
-
-  // socket
-  socket.on("welcome", async () => {
-    myDataChannel = myPeerConnection.createDataChannel("chat");
-    myDataChannel.addEventListener("message", (e) => console.log(e.data));
-    console.log("made data channel");
-
-    const offer = await myPeerConnection.createOffer();
-    await myPeerConnection.setLocalDescription(offer);
-    console.log("sent the offer");
-    socket.emit("offer", offer, roomId);
-  });
-
-  socket.on("offer", async (offer) => {
-    myPeerConnection.addEventListener("datachannel", (e) => {
-      myDataChannel = e.channel;
-      myDataChannel.addEventListener("message", (e) => console.log(e.data));
-    });
-    console.log("receive the offer");
-
-    await myPeerConnection.setRemoteDescription(offer);
-    const answer = await myPeerConnection.createAnswer();
-    await myPeerConnection.setLocalDescription(answer);
-    socket.emit("answer", answer, roomId);
-    console.log("sent the answer");
-  });
-
-  socket.on("answer", async (answer) => {
-    console.log("receive the answer");
-    await myPeerConnection.setRemoteDescription(answer);
-  });
-
-  socket.on("ice", async (ice) => {
-    console.log("received candidate");
-    await myPeerConnection.addIceCandidate(ice);
-  });
 
   return (
     <Container>
