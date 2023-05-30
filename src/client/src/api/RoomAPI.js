119cb46621be39972102bc905f3abc3b96790769
j8won <joowoni99@gmail.com>
2023-05-30 13:15:38 +0900

    feat: 방 생성하기 api 연결

--- src/client/src/api/RoomAPI.js
+++ src/client/src/api/RoomAPI.js
@@ -7,4 +7,24 @@ const getUserEnteredRoom = async () => {
   });
 };
 
-export default { getUserEnteredRoom };
+const createRoom = async (data) => {
+  let params = {
+    title: data.roomName,
+    password: data.roomPassword,
+  };
+  const { responseData } = await ApiController({
+    url: "/api/rooms/",
+    method: "POST",
+    data: params,
+  });
+
+  return responseData;
+};
+
+const enterRoom = async (data) => {
+  await ApiController({
+    url: `api/rooms/${data.roomId}/?password=${data.password}`,
+  });
+};
+
+export default { getUserEnteredRoom, createRoom };
